040bf37660e152c9d34b31e8f196c847d8e72859
CovertBlueberry <82067589+CovertBlueberry@users.noreply.github.com>
2021-04-19 12:41:52 -0700

    first layout

--- keyboards/lily58/keymaps/covertblueberry/config.h
+++ keyboards/lily58/keymaps/covertblueberry/config.h
@@ -26,21 +26,40 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+// https://precondition.github.io/home-row-mods
+// https://docs.qmk.fm/#/tap_hold
+// https://github.com/qmk/qmk_firmware/pull/9404/files
+// https://cdn.discordapp.com/attachments/663573863480950808/757162393209012304/modtap.pdf
 
-#undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 27
-#define RGBLIGHT_LIMIT_VAL 120
-#define RGBLIGHT_HUE_STEP 10
-#define RGBLIGHT_SAT_STEP 17
-#define RGBLIGHT_VAL_STEP 17
+// default 200
+#define TAPPING_TERM 200
+// #define TAPPING_TERM_PER_KEY
 
-// Underglow
-/*
-#undef RGBLED_NUM
-#define RGBLED_NUM 14    // Number of LEDs
-#define RGBLIGHT_ANIMATIONS
-#define RGBLIGHT_SLEEP
-*/
+// https://precondition.github.io/home-row-mods#ignore-mod-tap-interrupt
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// https://precondition.github.io/home-row-mods#tapping-force-hold
+// https://docs.qmk.fm/#/tap_hold?id=tapping-force-hold
+#define TAPPING_FORCE_HOLD_PER_KEY
+
+// Apply the modifier on keys that are tapped during a short hold of a modtap
+// (lets tapping a letter while holding down dual-function mod (for less than tapping term)
+// count as a mod+letter, rather than two letters)
+// can also do PERMISSIVE_HOLD_PER_KEY to enable e.g. only for shift
+// https://precondition.github.io/home-row-mods#permissive-hold
+// #define PERMISSIVE_HOLD
+
+// enables long hold and release of a dual-function mod to send the original key
+// (likely to cause problems with home row mods)
+// https://precondition.github.io/home-row-mods#retro-tapping
+// https://docs.qmk.fm/#/tap_hold?id=retro-tapping
+// #define RETRO_TAPPING
+
+// #define TAPPING_FORCE_HOLD
+
+// auto shift: hold a key longer to use shifted version
+// https://docs.qmk.fm/#/feature_auto_shift
+
+// retro shift: hold the mod-tap longer to get the shifted state
+// https://github.com/manna-harbour/qmk_firmware/blob/retro-shift/docs/tap_hold.md#retro-shift
+// https://precondition.github.io/home-row-mods#retro-shift
--- keyboards/lily58/keymaps/covertblueberry/keymap.c
+++ keyboards/lily58/keymaps/covertblueberry/keymap.c
@@ -1,52 +1,73 @@
 #include QMK_KEYBOARD_H
+#include <stdio.h>
+// #include "lily58/rev1/rev1.h"
+
+// Left-hand home row mods
+#define HOME_A LCTL_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LGUI_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
+
+// Right-hand home row mods
+#define HOME_J RSFT_T(KC_J)
+#define HOME_K RGUI_T(KC_K)
+#define HOME_L LALT_T(KC_L)
+#define HOME_SEMI RCTL_T(KC_SCLN)
 
 enum layer_number {
-  _QWERTY = 0,
-  _SYM,
-  _MEDIA,
+    _QWERTY = 0,
+    _SYM,
+    _MEDIA,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
+ * | ESC  |  1   |  2   |  3   |  4   |  5   |                    |   6  |   7  |   8  |   9  |   0  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
+ * |  `   |  Q   |  W   |  E   |  R   |  T   |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  `   |  A⇧  |  S⌥  |  D🍎 |  F 1 |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * | Tab  |  A ^ |  S ⌥ |  D * |  F ⇧ |  G   |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------|  del  |    | bksp  |------+------+------+------+------+------|
+ * |LShift|  Z   |  X   |  C   |  V   |  B   |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |Del   | /Space  /       \Enter \  |Bksp  | RGUI | RAlt |
+ *                   | caps |  del | sym  | /Enter  /       \Space \  |  sym | bksp | media|
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
-
+//LT(_SYM,???)
 [_QWERTY] = LAYOUT( \
-  KC_ESC,   KC_1,         KC_2,         KC_3,         KC_4,       KC_5,                     KC_6,         KC_7,       KC_8,         KC_9,         KC_0,             KC_MINS, \
-  KC_TAB,   KC_Q,         KC_W,         KC_E,         KC_R,       KC_T,                     KC_Y,         KC_U,       KC_I,         KC_O,         KC_P,             KC_BSLS, \
-  KC_GRAVE, LSFT_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LT(1,KC_F), LCTL_T(KC_G),             RCTL_T(KC_H), LT(1,KC_J), RGUI_T(KC_K), RALT_T(KC_L), RSFT_T(KC_SCOLON),KC_QUOTE,
-  KC_LSFT,  KC_Z,         KC_X,         KC_C,         KC_V,       KC_B,    KC_CAPS, MO(2),  KC_N,         KC_M,       KC_COMM,      KC_DOT,       KC_SLSH,          KC_RSFT, \
-                            KC_LALT,      KC_LGUI,      KC_DEL,     KC_SPC,               KC_ENT,       KC_BSPC,    KC_RGUI,      KC_RALT
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,   KC_5,                 KC_6,    KC_7,   KC_8,    KC_9,   KC_0,     KC_MINS, \
+  KC_GRAVE, KC_Q,   KC_W,    KC_E,    KC_R,   KC_T,                 KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,     KC_BSLS, \
+  KC_TAB,   HOME_A, HOME_S,  HOME_D,  HOME_F, KC_G,                 KC_H,    HOME_J, HOME_K,  HOME_L, HOME_SEMI,KC_QUOTE,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,   KC_B, KC_DEL,KC_BSPC, KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT, \
+                      KC_CAPS, KC_DEL, TT(_SYM),KC_ENT,          KC_SPC, TT(_SYM), KC_BSPC, MO(_MEDIA)
 ),
 /* SYM
+ * spc = space
+ * wrd = word
+ *
+ * mouse keys?
+ * (requires enabling MOUSEKEY_ENABLE in rules.mk)
+ * mw = mouse wheel: KC_WH_D, KC_WH_L, KC_WH_R, KC_WH_U
+ * mouse left click for click and drag?
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | spL  | spR  |  up  | home | end  | pgUp |                    |   $  |   {  |   }  |   =  |   +  | F12  |
+ * | spcL | wrdL |  up  | wrdR |      | pgUp |                    |   #  |   {  |   }  |   =  |   +  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      | left | down | rght |      | pgDn |-------.    ,-------|   (  |      |   )  |   -  |   _  |   `  |
- * |------+------+------+------+------+------|       |    |    ]  |------+------+------+------+------+------|
- * |      | mwDn | mwLf | mwRt | mwUp |      |-------|    |-------|      |   [  |   ]  |   \  |   |  |   ~  |
+ * | spcR | left | down | rght |      | pgDn |-------.    ,-------|   $  |   (  |   )  |   -  |   _  |   `  |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------|    |-------|   &  |   [  |   ]  |   \  |   |  |   ~  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  */
 
 [_SYM] = LAYOUT( \
-  _______,       KC_F1,         KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-  LCAG(KC_LEFT), LCAG(KC_RGHT), KC_UP,   KC_HOME, KC_END,  KC_PGUP,                   KC_DLR,  KC_LCBR, KC_RCBR, KC_EQL,  KC_PLUS, KC_F12, \
-  _______,       KC_LEFT,       KC_DOWN, KC_RGHT, _______, KC_PGDN,                   KC_LPRN, _______, KC_RPRN, KC_MINS, KC_UNDS, KC_GRV, \
-  _______,       KC_WH_D,       KC_WH_L, KC_WH_R, KC_WH_U, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_BSLS, KC_PIPE, KC_TILD, \
+  _______,      KC_F1,        KC_F2,   KC_F3,        KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
+  LCAG(KC_LEFT),LALT(KC_LEFT),KC_UP,   LALT(KC_RGHT),KC_HOME, KC_PGUP,                   KC_HASH, KC_LCBR, KC_RCBR, KC_EQL,  KC_PLUS, KC_F12, \
+  LCAG(KC_RGHT),KC_LEFT,      KC_DOWN, KC_RGHT,      KC_END,  KC_PGDN,                   KC_DLR,  KC_LPRN, KC_RPRN, KC_MINS, KC_UNDS, KC_GRV, \
+  _______,      _______,      _______, _______,      _______, _______, _______, _______, KC_AMPR, KC_LBRC, KC_RBRC, KC_BSLS, KC_PIPE, KC_TILD, \
   _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
@@ -54,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      | vlUp  |      |      |      |                    |      |      |      |      |      |      |
+ * |      |      | vlUp |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      | vlDn |      |      |      |-------.    ,-------|      |      |      |      |      |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
@@ -70,52 +91,116 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 )
 };
 
-// layer_state_t layer_state_set_user(layer_state_t state) {
-//  return update_tri_layer_state(state, _SYM, _RAISE, _ADJUST);
+bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case HOME_A:
+        case HOME_S:
+        case HOME_D:
+        case HOME_F:
+        case HOME_J:
+        case HOME_K:
+        case HOME_L:
+        case HOME_SEMI:
+            return true;
+        default:
+            return false;
+    }
+}
+
+// uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+//   switch (keycode) {
+//     case HOME_A:
+//     case HOME_S:
+//     case HOME_D:
+//     case HOME_F:
+//     case HOME_J:
+//     case HOME_K:
+//     case HOME_L:
+//     case HOME_SEMI:
+//       return 500;
+//     default:
+//       return TAPPING_TERM;
+//   }
 // }
 
-//SSD1306 OLED update loop, make sure to enable OLED_DRIVER_ENABLE=yes in rules.mk
+// layer_state_t layer_state_set_user(layer_state_t state) {
+//   return update_tri_layer_state(state, _SYM, _RAISE, _ADJUST);
+// }
+
+// copied from layer_state_reader.c
+#define L_BASE 0
+#define L_SYM (1 << 1)
+#define L_MEDIA (1 << 2)
+
+char layer_state_str[24];
+
+const char *read_layer_state(void) {
+    switch (layer_state) {
+        case L_BASE:
+            // return "Default";
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
+            break;
+        case L_SYM:
+            // return "Sym/Nav";
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Sym/Nav");
+            break;
+        case L_MEDIA:
+            // return "Media";
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Media");
+            break;
+        default:
+            // return "Other";
+              snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
+    }
+
+    return layer_state_str;
+}
+
+// SSD1306 OLED update loop, make sure to enable OLED_DRIVER_ENABLE=yes in rules.mk
 #ifdef OLED_DRIVER_ENABLE
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_keyboard_master())
-    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-  return rotation;
+    if (!is_keyboard_master()) return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+    return rotation;
 }
 
 // When you add source files to SRC in rules.mk, you can use functions.
-const char *read_layer_state(void);
+// const char *read_layer_state(void);
 const char *read_logo(void);
-void set_keylog(uint16_t keycode, keyrecord_t *record);
+void        set_keylog(uint16_t keycode, keyrecord_t *record);
 const char *read_keylog(void);
 const char *read_keylogs(void);
 
-// const char *read_mode_icon(bool swap);
-// const char *read_host_led_state(void);
 // void set_timelog(void);
 // const char *read_timelog(void);
 
-void oled_task_user(void) {
-  if (is_keyboard_master()) {
-    // If you want to change the display of OLED, you need to change here
-    oled_write_ln(read_layer_state(), false);
-    oled_write_ln(read_keylog(), false);
-    oled_write_ln(read_keylogs(), false);
-    //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
-    //oled_write_ln(read_host_led_state(), false);
-    //oled_write_ln(read_timelog(), false);
-  } else {
-    oled_write(read_logo(), false);
-  }
+char wpm_str[24];
+
+const char *read_wpm(void) {
+    snprintf(wpm_str, sizeof(wpm_str), "WPM: %i", get_current_wpm());
+    return wpm_str;
 }
-#endif // OLED_DRIVER_ENABLE
+
+void oled_task_user(void) {
+    if (is_keyboard_master()) {
+        // If you want to change the display of OLED, you need to change here
+        oled_write_ln(read_layer_state(), false);
+        oled_write_ln(read_keylog(), false);
+        oled_write_ln(read_keylogs(), false);
+        oled_write_ln(read_wpm(), false);
+        // oled_write_ln(read_timelog(), false);
+    } else {
+        oled_write(read_logo(), false);
+    }
+}
+#endif  // OLED_DRIVER_ENABLE
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
+    if (record->event.pressed) {
 #ifdef OLED_DRIVER_ENABLE
-    set_keylog(keycode, record);
+        set_keylog(keycode, record);
 #endif
-    // set_timelog();
-  }
-  return true;
+        // set_timelog();
+    }
+    return true;
 }
